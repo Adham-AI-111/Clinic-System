9c0dcde16514cbb59b0db4d61aeb1ece2b077856
adham <adhmslaha2@gmail.com>
2025-11-10 19:10:21 +0200

    first commit > the structure of the project

---
+++ doctor/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# will contain all the system details (appointments, patients, prescriptions, diagnoses), and pages for the system admin
---
+++ patient/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# will contain only the views that are related to the patient outside the clinic
+# read the nfc card and get the patient id, then redirect to the patient page
+# the patient page will also contain the ability to view the patient's medical history (giagonsis, prescriptions)
